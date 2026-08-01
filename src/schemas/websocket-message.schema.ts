8b176c981872b7bb6412f7128490bdbf8c97dd25
Brayan Landa <landakkpp@gmail.com>
2026-07-31 20:17:26 -0500

    refactor: update websocket message schema to use discriminated union and improve payload structure

--- src/schemas/websocket-message.schema.ts
+++ src/schemas/websocket-message.schema.ts
@@ -1,21 +1,34 @@
 import { z } from 'zod';
 
-const typeSchema = z.enum([
-  'GET_ITEMS',
-  'ADD_ITEM',
-  'UPDATE_ITEM',
-  'DELETE_ITEM',
+const latLngSchema = z.object({
+  lat: z.number('Latitude is required'),
+  lng: z.number('Longitude is required'),
+});
+
+export const messageSchema = z.discriminatedUnion('type', [
+  z.object({
+    type: z.literal('GET_CLIENTS'),
+    payload: z.undefined().optional()
+  }),
+  z.object({
+    type: z.literal('CLIENT_REGISTER'),
+    payload: z.object({
+      clientId: z.string('Client ID is required').min(1),
+      name: z.string('Client name is required').min(2),
+      color: z.string('Client color is required').min(1).optional(),
+      coords: latLngSchema,
+    })
+  }),
+  z.object({
+    type: z.literal('CLIENT_MOVE'),
+    payload: z.object({
+      clientId: z.string('Client ID is required').min(1),
+      coords: latLngSchema,
+    })
+  })
 ]);
 
-const payloadSchema = z.object({
-  id: z.string().optional(),
-  name: z.string().optional(),
-});
-
-export const messageSchema = z.object({
-  type: typeSchema,
-  payload: payloadSchema.optional(),
-});
-
 export type MessageParsed = z.infer<typeof messageSchema>;
-export type MessagePayload = z.infer<typeof payloadSchema>;
+export type ClientRegisterPayload = Extract<z.infer<MessageParsed>, { type: 'CLIENT_REGISTER' }>['payload'];
+export type ClientMovePayload = Extract<z.infer<MessageParsed>, { type: 'CLIENT_MOVE' }>['payload'];
+export type GetClientsPayload = Extract<z.infer<MessageParsed>, { type: 'GET_CLIENTS' }>['payload'];
