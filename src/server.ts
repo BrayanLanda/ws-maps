534f7c4003c358109f6f61c3707b00a1b396ee07
Brayan Landa <landakkpp@gmail.com>
2026-08-08 11:10:27 -0500

    feat: add client registration and disconnection handling in websocket server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { SERVER_CONFIG } from './config/server-config';
 import indexHtml from '../public/index.html';
 import { generateUuid } from './utils/generate-uuid';
 import type { WebSocketData } from './types';
-import { handleMessage } from './handlers/message.handler';
+import { handleClientLeft, handleClientRegister, handleMessage } from './handlers/message.handler';
 
 export const createServer = () => {
   const server = Bun.serve<WebSocketData>({
@@ -38,15 +38,16 @@ export const createServer = () => {
     },
     websocket: {
       open(ws) {
-        //! Una nueva conexión
-        // console.log(`Cliente: ${ws.data.clientId}`);
-        //! Suscribir el cliente a un canal por defecto
         ws.subscribe(SERVER_CONFIG.defaultChannelName);
-        // ! (opcional) Aquí se puede emitir el primer mensaje al cliente
-        // Emitir el primer mensaje al cliente que se acaba de conectar
-        // ws.send({ type: 'my_type', payload: { message: 'Some Payload' } });
-        //! Emitir el mensaje a todos los clientes conectados (-1 cliente que se acaba de conectar)
-        // ws.publish(SERVER_CONFIG.defaultChannelName, JSON.stringify(handleGetParties()));
+        const welcomeMessage = handleClientRegister(ws.data.clientId, ws.data);
+        
+        for (const personalMessage of welcomeMessage.personal) {
+          ws.send(JSON.stringify(personalMessage));
+        }
+
+        for (const broadcastMessage of welcomeMessage.broadcast) {
+          ws.publish(SERVER_CONFIG.defaultChannelName, JSON.stringify(broadcastMessage));
+        }
       },
       message(ws, message: string) {
         //* Todos los mensajes que llegan al servidor de la misma forma
@@ -61,15 +62,16 @@ export const createServer = () => {
         for (const broadcastMessage of response.broadcast) {
           ws.publish(SERVER_CONFIG.defaultChannelName, JSON.stringify(broadcastMessage));
         }
-
-        //! Si hay que enviar a todos los clientes conectados (publish + send)
-        // ws.send(responseString);
-        // ws.publish(SERVER_CONFIG.defaultChannelName, responseString);
       },
       close(ws, code, message) {
         //! Una vez que el cliente se desconecta, "de-suscribir" del canal por defecto
         // console.log(`Cliente desconectado: ${ws.data.clientId}`);
         ws.unsubscribe(SERVER_CONFIG.defaultChannelName);
+        const result = handleClientLeft(ws.data.clientId);
+
+        for (const broadcastMessage of result.broadcast) {
+          ws.publish(SERVER_CONFIG.defaultChannelName, JSON.stringify(broadcastMessage));
+        }
       }, // a socket is closed
     }, // handlers
   });
